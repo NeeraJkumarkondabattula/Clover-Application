c68fbfc19bd51e17ff08b9baefb8b6777a5ab0df
Neeraj Kumar <neerajkumarkondabattula@gmail.com>
2023-11-25 09:13:26 +0530

    update commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import DbConnection from "./config/mongoose.js";
+// import DbConnection from "../mongoose.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./route/userRoutes.js";
 import postRoutes from "./route/postRoutes.js";
@@ -10,18 +10,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 // DbConnection();
-mongoose
-  .connect(process.env.Mongourl, {
-    useNewUrlParser: true,
-    // useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to Mongo!");
-  })
-  .catch((err) => {
-    console.error("Error connecting to Mongo", err);
-  });
+
 const app = express();
 
 const port = process.env.PORT || 6767;
@@ -35,6 +24,19 @@ app.use(cors());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
+
+mongoose
+  .connect(process.env.Mongourl, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Mongo", err);
+  });
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
